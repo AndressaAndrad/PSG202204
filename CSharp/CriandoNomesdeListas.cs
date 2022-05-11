3210bef396974819a276d73410c21adfa71fca5e
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-05-11 17:12:12 -0400

    Create CriandoNomesdeListas.cs

---
+++ CSharp/CriandoNomesdeListas.cs
@@ -0,0 +1,30 @@
+        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap202204ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+       
+        public static List<string> CriarListaDeNomes()
+        {
+            List<string> listaDeNomes = new List<string>();
+            listaDeNomes.Add("Mateus");
+            listaDeNomes.Add("Lucas");
+            listaDeNomes.Add("João");
+            listaDeNomes.Add("Judas");
+            listaDeNomes.Add("Tadeu");
+            listaDeNomes.Add("Tiago");
+            listaDeNomes.Add("Marcos");
+            listaDeNomes.Add("André");
+            listaDeNomes.Add("Simão");
+            listaDeNomes.Add("Pedro");
+            listaDeNomes.Add("Tomé");
+            return listaDeNomes;
+        }
+         }  
+}
