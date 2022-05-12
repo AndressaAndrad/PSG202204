62ca2feac3131cbb9255efeb775912a216b5de59
Andressa Andrade <101049309+AndressaAndrad@users.noreply.github.com>
2022-05-12 14:14:43 -0400

    Update Desafio16a30.cs

--- CSharp/Desafio16a30.cs
+++ CSharp/Desafio16a30.cs
@@ -11,8 +11,8 @@
             static void Main(string[] args)
             {
 
-                Desafio_016();
-
+                //Desafio_016();
+                Desafio_017();
 
                 Console.ReadLine();
             }
@@ -34,3 +34,24 @@
                     Console.WriteLine("Número {0} é impar.", numero);
                 }
                 
+                 /// <summary>
+        /// Desafio 17 - Desenvolva um método que permita a entrada do nome do usuário e exiba
+        /// Imprima a frase “Olá meu nome é: {nome recebido}”.
+        /// O nome com todas as letras maiúsculas e minúsculas.
+        /// Quantas letras ao todo (sem considerar espaços).
+        /// Quantas letras tem o primeiro nome.
+        /// </summary>
+        public static void Desafio_017()
+        {
+            Console.Write("Qual é seu nome? ");
+            string nome = Console.ReadLine();
+            Console.Write("Qual é o seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine("Olá meu nome é: {0} {1} ", nome.ToUpper(), sobrenome.ToUpper());
+            Console.WriteLine("Olá meu nome é: {0} {1}", nome.ToLower(), sobrenome.ToLower());
+            Console.WriteLine("Com total de letras de : {0}.", nome.Count() + sobrenome.Count());
+            Console.WriteLine("O nome {0} tem {1} letras.", nome, nome.Count());
+
+
+        }
